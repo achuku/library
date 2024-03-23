d3b4b75232e30bfed926e54afb4469c16e3caf0b
Anyamba <achuku@gmail.com>
2024-03-23 18:16:45 +0300

    Amend constructor logic

--- library.js
+++ library.js
@@ -1,17 +1,25 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read){
+function Book(title, author, pages, isRead){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-    this.info = () => {
-        console.log(this.title, this.author, this.pages, this.read) ;
+    this.isRead = isRead;
+    let read;
+        if(!isRead){
+            read = 'not read yet';
+        } else if (isRead) {
+            read = 'read';  
+        } 
+    this.info = function(){
+        return (`${this.title} by ${this.autho}, ${this.pages}, ${read} `);
+    }
+        
     };
-} 
 
-    const book1 = new Book('The Hobbit', 'by J.R.R Tolkien,', '295 pages,', 'not read yet');
+
+
+    const book1 = new Book('The Hobbit', 'by J.R.R Tolkien', '295 pages', 'read'  );
     myLibrary.push(book1);
-
 
 console.log(myLibrary);
