c7b9dfdfd232d7ee68246cd5618fb002ec3d806e
Anyamba <achuku@gmail.com>
2024-03-26 15:13:49 +0300

    Add return in place of console.log in constructor

--- library.js
+++ library.js
@@ -6,13 +6,23 @@ function Book(title, author, pages, read){
     this.pages = pages;
     this.read = read;
     this.info = () => {
-        console.log(this.title, this.author, this.pages, this.read) ;
+        return (`${this.title} by ${this.author}, ${this.pages}, ${this.read}`) ;
     };
 } 
 
-    const book1 = new Book('The Hobbit', 'by J.R.R Tolkien,', '295 pages,', 'not read yet');
+    const book1 = new Book('The Hobbit', 'J.R.R Tolkien', '295 pages', 'not read yet');
     myLibrary.push(book1);
-    const book2 = new Book('Basic Life Skills for Success', 'by Sumbye Kapena', '96 pages', 'read');
-    myLibrary.push(book2);
+    console.log(book1.info());
+    // const book2 = new Book('Basic Life Skills for Success', 'by Sumbye Kapena', '96 pages', 'read');
+    // myLibrary.push(book2);
+    
+    // function addBookToLibrary() {
+    //     const newBook = new Book(title, author, pages, read);
+    //     myLibrary.push(newBook);
+    //     // title.value = '';
+    //     // author.value = '';
+    //     // pages.value = '';
+    // }
+    // addBookToLibrary('The Hobbit', 'by J.R.R Tolkien,', '295 pages,', 'not read yet');
 
 console.log(myLibrary);
