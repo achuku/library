6e7c0bffb65a50f1e0dcfd829a5e46c9b088236e
Anyamba <achuku@gmail.com>
2024-03-26 20:48:15 +0300

    Amend logic to make addBookToLibrary() functional

--- library.js
+++ library.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 
+//Constructor
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -10,32 +11,30 @@ function Book(title, author, pages, read){
     };
 } 
 
-    const book1 = new Book('The Hobbit', 'J.R.R Tolkien', '295 pages', 'not read yet');
-    myLibrary.push(book1);
-    //console.log(book1.info());
-    const book2 = new Book('Basic Life Skills for Success', 'Sumbye Kapena', '96 pages', 'read');
-    myLibrary.push(book2);
-    
-    // function addBookToLibrary() {
-    //     const newBook = new Book(title, author, pages, read);
-    //     myLibrary.push(newBook);
-        // title.value = '';
-        // author.value = '';
-        // pages.value = '';
-    //}
-    //addBookToLibrary('The Hobbit', 'by J.R.R Tolkien,', '295 pages,', 'not read yet');
+    //Add book to library
+    function addBookToLibrary(title, author, pages, read) {
+        const newBook = new Book(title, author, pages, read);
+        myLibrary.push(newBook);
+    }
+    addBookToLibrary('The Hobbit', 'by J.R.R Tolkien,', '295 pages,', 'not read yet');
 
 console.log(myLibrary);
 
 //Display books in library
 let book = '';
-myLibrary.forEach(displayItem);
+myLibrary.forEach(displayBook);
 
-function displayItem(item, index){
+function displayBook(item, index){
    book += index + ':' + item + '<br>';
    document.getElementById('display').innerHTML = book;
    console.log(index, item);
 }
+
+//Buttons
+const addNewBook = document.getElementById('.addBook');
+
+
+
 
 
 
